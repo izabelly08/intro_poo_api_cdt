7afc5e92db347a4578bd1dd928f6b20403453a63
igor freitas <291902050+gabrielcsilva009-creator@users.noreply.github.com>
2026-08-18 16:40:48 -0300

    update_modulo08

---
+++ modulo08_meudesenho_izabelly.py
@@ -0,0 +1,28 @@
+'''
+monica - carminha frufru
+barbie  - barbie
+sam&cat - sam
+'''
+
+
+class BonecoToyStory:
+
+    def __init__(self, nome, dono, frase_de_efeito):
+        self.nome = nome
+        self.dono = dono
+        self.frase_de_efeito = frase_de_efeito
+
+
+jessie = BonecoToyStory(
+    nome = "jessie",
+    dono = "Emily",
+    frase_de_efeito = "Preparem-se para a encrenca!"
+    
+)
+
+
+buzz ligthyear = BonecoToyStory(
+    nome = "buzz ligthyear",
+    dono = "andy",
+    frase_de_efeito = "ao infinito e alem"
+)
